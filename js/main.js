706c96807d92ae6c8a6d026431547bf23b5f8c2c
KhasanDev <63578294+hasanscsds@users.noreply.github.com>
2022-05-19 14:08:28 +0500

    res

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+   $("#hamburger").click(function(){
+      $(".res__nav").css("display", "inherit")
+   })
+   $("#exit-btn").click(function(){
+      $(".res__nav").css("display", "none")
+   })
+});
+//OWL CAROUSEL
+$('.owl-carousel').owlCarousel({
+   loop:true,
+   nav:false,
+   dots: true,
+   responsiveClass:true,
+   autoplay: true,
+   autoplaySpeed: 2000,
+   lazyContent: true,
+   autoplayHoverPause: true,
+   mouseDrag: true,
+   touchDrag: true,
+   responsive:{
+      0:{
+         items:1,
+      },
+      600:{
+         items:1,
+      },
+      1000:{
+         items:1,
+      }
+   }
+})
+
+$('features__and__pricing__container .owl-carousel').owlCarousel({
+
+   responsive:{
+      0:{
+         items:2,
+      },
+      479:{
+         items:2,
+         loop:false,
+         nav:false,
+         dots: false,
+         responsiveClass:true,
+         autoplay: false,
+         lazyContent: true,
+         autoplayHoverPause: true,
+         mouseDrag: true,
+         touchDrag: true,
+      },
+      1000:{
+         items:1,
+      }
+   }
+})
